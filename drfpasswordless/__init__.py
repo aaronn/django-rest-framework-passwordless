260dbe0c7144ea07345503aa5cda68632c47f7e8
aaronykng <aaronykng@gmail.com>
2017-03-26 12:02:35 -0700

    Tox

--- drfpasswordless/__init__.py
+++ drfpasswordless/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'drfpasswordless'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __author__ = 'Aaron Ng'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 Aaron Ng'
--- setup.py
+++ setup.py
@@ -1,13 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import shutil
+import sys
+
 from distutils.core import setup
 
-setup(
-    name='drfpasswordless',
-    version='0.1.0',
-    packages=['tests', 'drfpasswordless', 'drfpasswordless.templates'],
-    url='https://github.com/aaronn/django-rest-framework-passwordless',
-    license='MIT',
-    author='Aaron Ng',
-    author_email='hi@aaron.ng',
-    description='Passwordless auth for Django Rest Framework Token Authentication.'
-)
 
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with open(os.path.join(package, '__init__.py'), 'rb') as init_py:
+        src = init_py.read().decode('utf-8')
+        return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
+
+
+name = 'drfpasswordless'
+version = get_version('drfpasswordless')
+package = 'drfpasswordless'
+description = 'Passwordless auth for Django Rest Framework Token Authentication.'
+url = 'https://github.com/aaronn/django-rest-framework-passwordless'
+author = 'Aaron Ng'
+author_email = 'hi@aaron.ng'
+license = 'MIT'
+install_requires = []
+
+
+def read(*paths):
+    """
+    Build a file path from paths and return the contents.
+    """
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == 'publish':
+    if os.system('pip freeze | grep wheel'):
+        print('wheel not installed.\nUse `pip install wheel`.\nExiting.')
+        sys.exit()
+    if os.system('pip freeze | grep twine'):
+        print('twine not installed.\nUse `pip install twine`.\nExiting.')
+        sys.exit()
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('drfpasswordless.egg-info')
+    print('You probably want to also tag the version now:')
+    print("  git tag -a {0} -m 'version {0}'".format(version))
+    print('  git push --tags')
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=version,
+    url=url,
+    license=license,
+    description=description,
+    long_description=read('README'),
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
