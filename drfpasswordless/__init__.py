9eb25575ef70aa7628196df35af7b38111db2f1b
Aaron <aaronykng@gmail.com>
2019-02-26 15:21:27 -0800

    pypi

--- drfpasswordless/__init__.py
+++ drfpasswordless/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'drfpasswordless'
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __author__ = 'Aaron Ng'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2018 Aaron Ng'
+__copyright__ = 'Copyright 2019 Aaron Ng'
 
 # Version synonym
 VERSION = __version__
