2f2f089a14d71afe7ad237711f9218aeb6699a29
Aaron <aaronykng@gmail.com>
2020-01-22 00:50:54 -0800

    Fix urls

--- tests/urls.py
+++ tests/urls.py
@@ -7,11 +7,11 @@ from drfpasswordless.views import (ObtainEmailCallbackToken,
                                    ObtainEmailVerificationCallbackToken,
                                    ObtainMobileVerificationCallbackToken, )
 
-urlpatterns = [url(r'^callback/auth/$', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_callback'),
+urlpatterns = [url(r'^auth/token/$', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_callback'),
                url(r'^auth/email/$', ObtainEmailCallbackToken.as_view(), name='auth_email'),
                url(r'^auth/mobile/$', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
-               url(r'^callback/verify/$', VerifyAliasFromCallbackToken.as_view(), name='verify_callback'),
-               url(r'^verify/email/$', ObtainEmailVerificationCallbackToken.as_view(), name='verify_email'),
-               url(r'^verify/mobile/$', ObtainMobileVerificationCallbackToken.as_view(), name='verify_mobile')]
+               url(r'^auth/verify/$', VerifyAliasFromCallbackToken.as_view(), name='verify_callback'),
+               url(r'^auth/verify/email/$', ObtainEmailVerificationCallbackToken.as_view(), name='verify_email'),
+               url(r'^auth/verify/mobile/$', ObtainMobileVerificationCallbackToken.as_view(), name='verify_mobile')]
 
 format_suffix_patterns(urlpatterns)
