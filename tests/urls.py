fdb67705d5990cd2da4be57c76cd0dbbb46a85a8
Aaron <aaronykng@gmail.com>
2020-01-23 23:33:45 -0800

    Fix tests for urls

--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from drfpasswordless.settings import api_settings
 from drfpasswordless.views import (ObtainEmailCallbackToken,
                                    ObtainMobileCallbackToken,
                                    ObtainAuthTokenFromCallbackToken,
@@ -7,11 +8,16 @@ from drfpasswordless.views import (ObtainEmailCallbackToken,
                                    ObtainEmailVerificationCallbackToken,
                                    ObtainMobileVerificationCallbackToken, )
 
-urlpatterns = [url(r'^auth/token/$', ObtainAuthTokenFromCallbackToken.as_view(), name='auth_callback'),
-               url(r'^auth/email/$', ObtainEmailCallbackToken.as_view(), name='auth_email'),
-               url(r'^auth/mobile/$', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
-               url(r'^auth/verify/$', VerifyAliasFromCallbackToken.as_view(), name='verify_callback'),
-               url(r'^auth/verify/email/$', ObtainEmailVerificationCallbackToken.as_view(), name='verify_email'),
-               url(r'^auth/verify/mobile/$', ObtainMobileVerificationCallbackToken.as_view(), name='verify_mobile')]
+urlpatterns = [
+    path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'email/', ObtainEmailCallbackToken.as_view(), name='auth_email'),
+    path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'mobile/', ObtainMobileCallbackToken.as_view(), name='auth_mobile'),
+    path(api_settings.PASSWORDLESS_AUTH_PREFIX + 'token/', ObtainAuthTokenFromCallbackToken.as_view(),
+         name='auth_token'),
+    path(api_settings.PASSWORDLESS_VERIFY_PREFIX + 'email/', ObtainEmailVerificationCallbackToken.as_view(),
+         name='verify_email'),
+    path(api_settings.PASSWORDLESS_VERIFY_PREFIX + 'mobile/', ObtainMobileVerificationCallbackToken.as_view(),
+         name='verify_mobile'),
+    path(api_settings.PASSWORDLESS_VERIFY_PREFIX, VerifyAliasFromCallbackToken.as_view(), name='verify_token'),
+]
 
 format_suffix_patterns(urlpatterns)
