92be2e2b5be6739bc67cab1a0cb9deb919777c1b
Eden-Eliel <61108318+Eden-Eliel@users.noreply.github.com>
2020-04-27 21:23:02 +0300

    Update services.py

--- drfpasswordless/services.py
+++ drfpasswordless/services.py
@@ -1,3 +1,5 @@
+from django.utils.module_loading import import_string
+from drfpasswordless.settings import api_settings
 from drfpasswordless.utils import (
     create_callback_token_for_user,
     send_email_with_callback_token,
@@ -9,11 +11,12 @@ class TokenService(object):
     @staticmethod
     def send_token(user, alias_type, token_type, **message_payload):
         token = create_callback_token_for_user(user, alias_type, token_type)
+
         send_action = None
         if alias_type == 'email':
-            send_action = send_email_with_callback_token
+            send_action = import_string(api_settings.PASSWORDLESS_AUTH_EMAIL_SEND_FUNCTION)
         elif alias_type == 'mobile':
-            send_action = send_sms_with_callback_token
+            send_action = import_string(api_settings.PASSWORDLESS_AUTH_MOBILE_SEND_FUNCTION)
         # Send to alias
         success = send_action(user, token, **message_payload)
         return success
